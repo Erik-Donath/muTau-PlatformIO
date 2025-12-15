a5b6c480218c8692ce8f056947b721cd6e76c5d1
Erik Donath <erikdonath@proton.me>
2025-12-15 19:16:22 +0100

    Blink

--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
 // src/main.c
 
 #include <generated/csr.h>
-#include <generated/soc.h>
-#include <generated/mem.h>
 
 int main(void) {
     while (1) {
         leds_out_write(0xAA);
+        for(int i = 0; i < 1000000; i++);
+        leds_out_write(0x55);
+        for(int i = 0; i < 1000000; i++);
     }
-
     return 0;
 }
